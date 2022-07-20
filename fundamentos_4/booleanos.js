daa28955f24cb189f8303d4a9342ee43b1d8dcf7
RoniDeringer <ronideringer@gmail.com>
2022-07-20 19:40:23 -0300

    20-07-22

--- fundamentos_4/booleanos.js
+++ fundamentos_4/booleanos.js
@@ -1,11 +1,16 @@
 let isAtivo = false
-console.log(isAtivo)
+console.log(isAtivo ,'\n')
 
 isAtivo = true
-console.log(isAtivo)
+console.log(isAtivo ,'\n')
 
-isAtivo = 1
-console.log(!!isAtivo)
+isAtivo = 5  // valor numerico
+console.log('valor logico tipo numerico: ',!!isAtivo ,'\n') //2 negação mostra o valor logico
+/**
+ * 0 é FALSE
+ * resto é TRUE
+ */
+
 
 console.log('os verdadeiros...') 
 console.log(!!3)
@@ -15,7 +20,7 @@ console.log(!!'texto')
 console.log(!![])
 console.log(!!{})
 console.log(!!Infinity)
-console.log(!!(isAtivo = true))
+console.log(!!(isAtivo = true),'\n')
 
 console.log('os falsos...')
 console.log(!!0)
@@ -23,10 +28,10 @@ console.log(!!'')
 console.log(!!null)
 console.log(!!NaN)
 console.log(!!undefined)
-console.log(!!(isAtivo = false))
+console.log(!!(isAtivo = false),'\n')
 
 console.log('pra finalizar...')
-console.log(!!('' || null || 0 || ' '))
+console.log(!!('' || null || 0 || ' '),'\n')
 Math.SQRT1_2
 
 let nome = 'Lucas'

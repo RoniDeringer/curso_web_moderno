e91988029f03fe58f5b38696eb3f57d9cf111316
RoniDeringer <ronideringer@gmail.com>
2022-07-20 19:17:34 -0300

    20-07-22

--- fundamentos_4/funcoes_string.js
+++ fundamentos_4/funcoes_string.js
@@ -3,7 +3,7 @@ console.log("10"/2 ,'\n')
 
 const texto = "Roni Deringer"
 console.log("charAt (posicao String)")
-console.log(texto.charAt(3))
+console.log(texto.charAt(3),'\n')
 
 console.log("substring (intervalo de carecteres) ")
 console.log(texto.substring(0, 4) ,'\n')
@@ -13,3 +13,7 @@ console.log('Texto '.concat(texto).concat("!") ,'\n')
 
 console.log("split (transforma em array (parametro o separador))")
 console.log('Ana,Breno, Pedro,José'.split(',') ,'\n')
+
+console.log("template ` (concatenação de varias string)")
+const up = nome => nome.toUpperCase()
+console.log(`Olá, ${up('Roni')}!`,'\n')
--- fundamentos_4/numeros_Tipos.js
+++ fundamentos_4/numeros_Tipos.js
@@ -34,19 +34,4 @@
     console.log(area)
     console.log(typeof Math)
 
-    console.log("Consigo dividir string por int:")
-    console.log("10"/2 ,'\n')
-
-    const texto = "Roni Deringer"
-    console.log("charAt (posicao String)")
-    console.log(texto.charAt(3))
-
-    console.log("substring (intervalo de carecteres) ")
-    console.log(texto.substring(0, 4) ,'\n')
-
-    console.log("Concat(concatena)")
-    console.log('Texto '.concat(texto).concat("!") ,'\n')
-
-    console.log("split (transforma em array (parametro o separador))")
-    console.log('Ana,Breno, Pedro,José'.split(',') ,'\n')
 }

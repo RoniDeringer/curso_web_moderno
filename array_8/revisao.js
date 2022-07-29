c9802ce99dccf90552c9e8365bf8a87b6d802718
RoniDeringer <ronideringer@gmail.com>
2022-07-28 22:35:39 -0300

    28-07-22

--- array_8/revisao.js
+++ array_8/revisao.js
@@ -7,15 +7,19 @@ console.log(nome.length) // 4
 
 console.log(nome.sort())
 
+
 //splice (elemento q vai ser alterado, quantidade de elementos excluidos, o q vai ser adicionado)
 
 let teste = new Array(1,2,3,4)
 teste.splice(1,2,'abc') // [ 1, 'abc', 4 ]
 
+
 teste = new Array(1,2,3,4)
 teste.splice(0,1,'abc') //[ 'abc', 2, 3, 4 ]
 
 
+teste = new Array(1,2,3,4)
+teste.splice(0,1,'abc','def') //[ 'abc', 'def', 2, 3, 4 ]
 
 
 
